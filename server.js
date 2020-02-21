74d316b4d2a77dd576a9bac8fe6fbfa0f67894b2
John <jcmeier22@gmail.com>
2020-02-20 21:11:55 -0500

    debug production

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(function(err, req, res, next) {
 });
 
 if(process.env.NODE_ENV === 'production'){
+  console.log('client url: ' + process.env.CLIENT)
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", process.env.CLIENT);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
